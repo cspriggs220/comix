f2e9feba29e430c38b6737455dfbfd19339644bb
Cody Spriggs <cody.spriggs@daveramsey.com>
2021-06-25 04:46:25 -0500

    WIP - ESLint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  extends: ['@ramseyinhouse'],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    // Use additional environments (ES6, jQuery, Jasmine, etc.)
+    // http://eslint.org/docs/user-guide/configuring#specifying-environments
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  globals: {
+    module: false
+  },
+  rules: {
+    // Add additional rules
+    'prefer-reflect': 0 // http://eslint.org/docs/rules/prefer-reflect
+  }
 };
